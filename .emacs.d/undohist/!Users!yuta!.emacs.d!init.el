84eaa75b97f603116ad9d2665cda2b04f185a6da
Nasu Yuta <yuta@nasuyuutanoMacBook-puro.local>
2019-01-23 22:52:42 +0900

    [emacs] Install latex-math-preview

--- .emacs.d/undohist/!Users!yuta!.emacs.d!init.el
+++ .emacs.d/undohist/!Users!yuta!.emacs.d!init.el
@@ -1,15 +1,15 @@
 
-((digest . "bad21c8ccb768725658207008a57382c") (undo-list (12062 . 12301) ("(custom-set-faces
+((digest . "aa4ea86d0931e0f9b2099129eec2b0d3") (undo-list (12128 . 12367) ("(custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )" . -12062) ((marker . 12062) . -239) ((marker . 12062) . -238) ((marker . 12062) . -239) ((marker . 12062) . -18) ((marker . 12062) . -18) ((marker . 12062) . -60) ((marker . 12062) . -60) ((marker . 12062) . -125) ((marker . 12062) . -125) ((marker . 12062) . -183) ((marker . 12062) . -183) ((marker . 12062) . -237) ((marker . 12062) . -237) (12059 . 12061) (11745 . 11749) (" " . 11745) ("
-" . -12056) (11745 . 11746) (12055 . 12056) (11744 . 11745) (" " . 11744) (11734 . 12055) (" " . 11734) (11461 . 11735) ("(custom-set-variables
+ )" . -12128) ((marker . 12128) . -239) ((marker . 12128) . -238) ((marker . 12128) . -239) (12125 . 12127) (11792 . 11796) (" " . 11792) ("
+" . -12122) (11792 . 11793) (12121 . 12122) (11791 . 11792) (" " . 11791) (11781 . 12121) (" " . 11781) (11508 . 11782) ("(custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (py-yapf python-mode flycheck dumb-jump undohist undo-tree markdown-preview-mode rainbow-mode kotlin-mode popwin slime exec-path-from-shell fish-mode yaml-mode web-mode scss-mode ruby-electric ruby-block recentf-ext pos-tip pkg-info neotree multi-term impatient-mode hiwin dash counsel company avy-migemo))))" . -11461) ((marker . 11461) . -22) ((marker . 11461) . -22) ((marker . 11461) . -68) ((marker . 11461) . -68) ((marker . 11461) . -133) ((marker . 11461) . -133) ((marker . 11461) . -191) ((marker . 11461) . -191) ((marker . 11461) . -245) ((marker . 11461) . -245) ((marker . 11461) . -274) ((marker . 11461) . -274) ((marker . 11461) . -284) ((marker . 11461) . -284) nil undo-tree-canary))
+    (ein py-yapf python-mode flycheck dumb-jump undohist undo-tree markdown-preview-mode rainbow-mode kotlin-mode popwin slime exec-path-from-shell fish-mode yaml-mode web-mode scss-mode ruby-electric ruby-block recentf-ext pos-tip pkg-info neotree multi-term impatient-mode hiwin dash counsel company avy-migemo))))" . -11508) ((marker . 11508) . -22) ((marker . 11508) . -22) ((marker . 11508) . -68) ((marker . 11508) . -68) ((marker . 11508) . -133) ((marker . 11508) . -133) ((marker . 11508) . -191) ((marker . 11508) . -191) ((marker . 11508) . -245) ((marker . 11508) . -245) ((marker . 11508) . -274) ((marker . 11508) . -274) ((marker . 11508) . -284) ((marker . 11508) . -284) nil (11104 . 11118) nil (11103 . 11104) nil (11086 . 11103) nil (11084 . 11086) nil (11081 . 11084) nil (11078 . 11081) nil (11076 . 11078) nil (11464 . 11465) nil undo-tree-canary))
